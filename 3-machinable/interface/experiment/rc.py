d794f36dbd0e330148fb0f4b6b02f20ca94ad443
Frithjof Gressmann <frithjof@noc.io>
2023-09-26 15:41:13 -0500

    Add simple simulation loop

--- 3-machinable/interface/experiment/rc.py
+++ 3-machinable/interface/experiment/rc.py
@@ -8,8 +8,9 @@ from miv_simulator import config
 from miv_simulator.utils import from_yaml
 from typing import Dict
 import logging
-from miv_simulator import lfp
+from miv_simulator.lfp import LFP
 from machinable.config import to_dict
+from miv_simulator.coding import cast_spike_times
 
 
 def mpi_excepthook(type, value, traceback):
@@ -49,13 +50,16 @@ class Reservoir(Component):
             filepath=self.config.cells,
             cell_types=to_dict(self.config.cell_types),  # TODO: remove dict conversion
             templates=self.config.templates,
-            io_size=0,
         )
 
-        # TODO: optostim
+        env.load_connections(
+            filepath=self.config.connections,
+            cell_filepath=self.config.cells,  # TODO: decouple
+            synapses=self.config.synapses,
+        )
 
-        lfp.LFP(
-            "Electrode 0",
+        lfp = LFP(
+            "ReadoutElectrode",
             env.pc,
             pop_gid_dict={
                 pop_name: set(env.cells[pop_name].keys()).difference(
@@ -63,21 +67,38 @@ class Reservoir(Component):
                 )
                 for pop_name in env.cells.keys()
             },
-            pos=[100.0, 100.0, 0.0],
+            pos=[500.0, 500.0, 0.0],  # top-center
             rho=333.0,
             dt_lfp=0.1,
             fdst=0.1,
-            maxEDist=100.0,
+            maxEDist=100.0,  # radius
             seed=self.seed + 1,
         )
 
-        env.load_connections(
-            filepath=self.config.connections,
-            cell_filepath=self.config.cells,
-            synapses=self.config.synapses,
-        )
-
-        # TODO: init_input_cells
-
         h.v_init = -65
         h.stdinit()
+        h.secondorder = 2  # crank-nicholson
+        h.dt = 0.025
+        env.pc.timeout(600.0)
+
+        h.finitialize(h.v_init)
+
+        env.pc.psolve(0.0)
+        for step in range(30):
+            if env.rank == 0 and step % 10 == 0:
+                print(f"Step {step} at t={h.t}")
+
+            # random stimulation
+            for gid, cell in env.artificial_cells["STIM"].items():
+                if not (env.pc.gid_exists(gid)):
+                    continue
+
+                uniform_spike_train = cast_spike_times(
+                    h.t + 1000 * np.random.uniform(size=10)
+                )
+                cell.play(h.Vector(uniform_spike_train))
+
+            env.pc.psolve(h.t + 1.0)
+
+        if env.rank == 0:
+            print("Electrode activity: ", list(zip(lfp.t, lfp.meanlfp)))

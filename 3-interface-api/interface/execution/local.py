646fe0c0713420ffc2fe1eff522a7d9fe5aae062
Frithjof Gressmann <frithjof@noc.io>
2023-12-06 11:12:38 -0600

    Use subprocess workaround for OpenMPI systems

--- 3-interface-api/interface/execution/local.py
+++ 3-interface-api/interface/execution/local.py
@@ -14,25 +14,32 @@ class LocalExecution(Execution):
         mpi: str = "mpirun"
 
     def __call__(self):
+        print("\n".join(self.pending_executables.map(lambda x: x.module)))
+        print(
+            f"\nSubmitting {len(self.pending_executables)} jobs ({len(self.executables)} total)."
+        )
         for executable in self.pending_executables:
-            if executable.config.get("ranks_", 0) > 0:
+            if executable.config.get("ranks", 0) > 0:
                 # run using MPI
                 script_file = self.save_file(
                     f"mpi-{executable.id}.sh",
-                    executable.dispatch_code(),
+                    "#!/usr/bin/env bash\n\n" + executable.dispatch_code(),
                 )
                 st = os.stat(script_file)
                 os.chmod(script_file, st.st_mode | stat.S_IEXEC)
                 cmd = [
                     shutil.which(self.config.mpi),
                     "-n",
-                    str(executable.config.ranks_),
+                    str(executable.config.get("ranks", 1)),
                     script_file,
                 ]
-                # capture output to file and stream it
+                print(" ".join(cmd))
                 with open(executable.local_directory("output.log"), "wb") as f:
                     p = subprocess.Popen(
-                        cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
+                        cmd,
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.STDOUT,
+                        env=os.environ,  # see https://stackoverflow.com/a/60070753
                     )
                     for o in iter(p.stdout.readline, b""):
                         sys.stdout.write(o.decode("utf-8"))
